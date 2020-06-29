c3a8f19207b373a40b037d919d5650988fb9e2ef
Maria Saavedra <msaav3@gmail.com>
2020-06-29 17:54:14 -0500

    added files...

---
+++ assets/js/index.js
@@ -0,0 +1,40 @@
+function buildString(){
+  let email = document.getElementById('to'),
+      to = email.value,
+      subject = encodeURIComponent(document.getElementById('subject').value),
+      body = encodeURIComponent(document.getElementById('body').value),
+      link = document.getElementById('link'),
+      message = ''
+  if (to){
+    email.className = 'not'
+    message = 'mailto:'+to
+    subject||body?message+='?':false
+    subject?message+='subject='+subject:false
+    subject&&body?message+='&body='+body:false
+    !subject&&body?message+='body='+body:false  
+    //link.innerHTML = message
+    sendRequest(message);
+  } else {
+    email.className='error'
+    alert('Please enter a recipient email address','error');
+    email.focus()
+  }
+}
+
+function sendRequest(msg){
+  // post request containing mailto string, and url.
+  link = document.getElementById('link');
+  axios.post('/templates', {
+    template: msg
+  })
+  .then(function (response) {
+    console.log(response.data.url);
+    let href =  'http://localhost:7000/templates/' + response.data.url;
+    link.href = href;
+    link.innerHTML = href;
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+}
---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const app = express();
+const shortid = require('shortid');
+const path = require("path");
+const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
+
+
+// ========================
+// Link to Database
+// ========================
+// Updates environment variables
+// @see https://zellwk.com/blog/environment-variables/
+
+// Replace process.env.DB_URL with your actual connection string
+const connectionString = "mongodb://localhost/mailtodb";
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('mailtodb')
+    const templatesCollection = db.collection('templates')
+
+    // ========================
+    // Middlewares
+    // ========================
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(bodyParser.json())
+    app.use(express.static('assets'));
+
+    // ========================
+    // Routes
+    // ========================
+    app.get('/', function(req, res) {
+      res.sendFile(path.join(__dirname + '/views/index.html'));
+    });
+
+    app.post('/templates', (req, res) => {
+      templatesCollection.insertOne({
+          url: shortid.generate(),
+          template: req.body.template
+      })
+        .then(result => {
+          res.json(result.ops[0]);
+        })  
+        .catch(error => console.error(error))
+    })
+
+
+    app.get('/templates/:id', (req, res) => {
+      db.collection('templates').findOne({"url": req.params.id })
+        .then(templates => {
+          res.redirect(templates.template);
+        });
+    })
+
+    // ========================
+    // Listen
+    // ========================
+
+    const port = 7000;
+    app.listen(port, function () {
+      console.log(`listening on ${port}`)
+    })
+  })
+  .catch(console.error)
